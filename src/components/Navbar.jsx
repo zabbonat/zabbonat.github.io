6617a20e0ca3ec1b5ef19898226de829ab90b309
PrinDDI <diletta.abbonato@unimib.it>
2025-11-27 21:31:26 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
         { name: 'Home', href: '#' },
         { name: 'About', href: '#about' },
         { name: 'Research', href: '#research' },
+        { name: 'Teaching', href: '#teaching' },
         { name: 'CV', href: '#cv' },
     ];
 
